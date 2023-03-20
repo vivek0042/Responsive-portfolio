80baa57699cb58ec9f9a94704565a17ea804cc60
vivek jain <100334280+vivek0042@users.noreply.github.com>
2023-03-20 08:39:12 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,43 @@
+import react from 'react'
+import { NavLink } from 'react-router-dom'
+import web from './images/vivek.jpeg'
+import './App.css'
+import About from './About'
+
+const Home=()=>{
+  return(
+    <>
+       <section id="header" className='d-flex align-items-center'>
+        <div className='container-fluid nav_bg'> 
+            <div className='row'>
+                <div className='col-10-mx-auto'>
+                  <div className='row'>
+                <div className='col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column'>
+                 <h1>
+                    Hi! I'm <strong className='brand-name'>Vivek Jain</strong>
+                 </h1>
+                 <h2 className='my-3'>   {/*my-3 here y is for top and bottom both */}
+                 Web Developer,ML Enthusiast,learner.
+                 </h2>
+                  <div className='mt-3'>    {/*mt-3 means margin top by 3 px */}
+                    <NavLink to="https://www.linkedin.com/in/vivek-jain-73b35a214/" className='btn-get-started'>Linkedin</NavLink>
+                    <NavLink to="https://github.com/vivek0042" className='btn-get-started'>Github</NavLink>
+                  
+                </div>
+
+                </div>
+                <div className='col-lg-6 order-1 order-lg-1  header-img'>  {/*in this order use for showing position ingrid .it is responsive element */}
+                    <img src={web} className="img-fluid animated" alt="homeimg"/>
+                </div>
+
+
+                </div>
+            </div> 
+        </div> 
+        </div>
+       </section>
+
+    </>
+  )
+}
+export default Home
